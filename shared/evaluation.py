800d6b828f7078d117a5d32ae264e0c343a579ab
Maggie Makar <maggieymakar@gmail.com>
2022-02-07 21:19:34 -0500

    updated the hsic function. This is the biased estimator with weighting. It's ok to use biased estimator because that is what most people use

--- shared/evaluation.py
+++ shared/evaluation.py
@@ -2,20 +2,20 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
 
-
-def compute_loss(labels, logits, z_pred, params):
+def compute_loss(labels, logits, embedding, sample_weights, params):
 	if params['weighted'] == 'False':
 		prediction_loss, hsic_loss = compute_loss_unweighted(labels, logits,
-			z_pred, params)
+			embedding, params)
 	else:
-		raise NotImplementedError("only implemented without weighting")
+		prediction_loss, hsic_loss = compute_loss_weighted(labels, logits,
+			embedding, sample_weights, params)
 	return prediction_loss, hsic_loss
 
 
-def compute_loss_unweighted(labels, logits, z_pred, params):
+def compute_loss_unweighted(labels, logits, embedding, params):
 	# labels: ground truth labels([y0(pnemounia), y1(sex), y2(support device)])
 	# logits: predicted label(pnemounia)
-	# z_pred: a learned representation vector
+	# embedding: a learned representation vector
 	y_main = tf.expand_dims(labels[:, 0], axis=-1)
 
 	individual_losses = tf.keras.losses.binary_crossentropy(y_main, logits,
@@ -24,38 +24,88 @@ def compute_loss_unweighted(labels, logits, z_pred, params):
 	unweighted_loss = tf.reduce_mean(individual_losses)
 	aux_y = labels[:, 1:]
 	if params['alpha'] > 0:
-		hsic_loss = hsic(z_pred, aux_y, sigma=params['sigma'])
+		hsic_loss = hsic(embedding, aux_y, sample_weights=None,
+			sigma=params['sigma'])
 	else:
 		hsic_loss = 0.0
 	return unweighted_loss, hsic_loss
 
 
-def hsic(x, y, sigma=1.0):
-	""" Computes the HSIC between two arbitrary variables x, y"""
+def compute_loss_weighted(labels, logits, embedding, sample_weights, params):
+	# labels: ground truth labels([y0(pnemounia), y1(sex), y2(support device)])
+	# logits: predicted label(pnemounia)
+	# embedding: a learned representation vector
+	y_main = tf.expand_dims(labels[:, 0], axis=-1)
+
+	individual_losses = tf.keras.losses.binary_crossentropy(y_main, logits,
+		from_logits=True)
+
+	weighted_loss = sample_weights * individual_losses
+	weighted_loss = tf.math.divide_no_nan(
+		tf.reduce_sum(weighted_loss),
+		tf.reduce_sum(sample_weights)
+	)
+
+	aux_y = labels[:, 1:]
+	if params['alpha'] > 0:
+		hsic_loss = hsic(embedding, aux_y, sample_weights=sample_weights,
+			sigma=params['sigma'])
+	else:
+		hsic_loss = 0.0
+	return unweighted_loss, hsic_loss
+
+
+def hsic(x, y, sample_weights=None, sigma=1.0):
+	""" Computes the weighted HSIC between two arbitrary variables x, y"""
 
 	if len(x.shape) == 1:
-		x = tf.expand_dims(x, axis=-1)
+			x = tf.expand_dims(x, axis=-1)
 
 	if len(y.shape) == 1:
-		y = tf.expand_dims(y, axis=-1)
+			y = tf.expand_dims(y, axis=-1)
+
+	if sample_weights == None:
+			sample_weights = tf.ones((tf.shape(y)[0], 1))
+
+	if len(sample_weights.shape) == 1:
+			sample_weights = tf.expand_dims(sample_weights, axis=-1)
+
+	sample_weights_T = tf.transpose(sample_weights)
+
 
 	kernel_fxx = tfp.math.psd_kernels.ExponentiatedQuadratic(
-		amplitude=1.0, length_scale=sigma)
+			amplitude=1.0, length_scale=sigma)
 
 	kernel_xx = kernel_fxx.matrix(x, x)
 	kernel_fyy = tfp.math.psd_kernels.ExponentiatedQuadratic(
-		amplitude=1.0, length_scale=sigma)
+			amplitude=1.0, length_scale=sigma)
 	kernel_yy = kernel_fyy.matrix(y, y)
 
-	tK = kernel_xx - tf.linalg.diag_part(kernel_xx)
-	tL = kernel_yy - tf.linalg.diag_part(kernel_yy)
+	N = tf.cast(tf.shape(y)[0], tf.float32)
 
-	N = y.shape[0]
-	hsic_term1 = tf.linalg.trace(tK @ tL)
-	hsic_term2 = tf.reduce_sum(tK) * tf.reduce_sum(tL) / (N -1) / (N - 2)
-	hsic_term3 = tf.tensordot(tf.reduce_sum(tK, 0), tf.reduce_sum(tL, 0), 1) / (N - 2)
+	# First term
+	hsic_1 = tf.math.multiply(kernel_xx, kernel_yy)
+	hsic_1 = tf.linalg.matmul(sample_weights_T, hsic_1)
+	hsic_1 = tf.linalg.matmul(hsic_1, sample_weights)
+	hsic_1 = hsic_1 / (N **2)
 
-	return (hsic_term1 + hsic_term2 - 2 * hsic_term3) / (N * (N - 3))
+	# Second term
+	# Note there is a typo in the paper. Authors will update
+	W_matrix = tf.linalg.matmul(sample_weights, sample_weights_T)
+	hsic_2 = tf.math.multiply(kernel_yy, W_matrix)
+	hsic_2 = tf.reduce_sum(hsic_2, keepdims=True) * tf.reduce_sum(kernel_xx, keepdims=True)
+	hsic_2 = hsic_2 / (N ** 4)
+
+	# third term
+	hsic_3 = tf.linalg.matmul(kernel_yy, sample_weights)
+	hsic_3 = tf.math.multiply(
+			tf.reduce_sum(kernel_xx, axis =1, keepdims=True), hsic_3)
+	hsic_3 = tf.linalg.matmul(sample_weights_T, hsic_3)
+	hsic_3 = 2 * hsic_3 / (N ** 3)
+
+	hsic_val = hsic_1 + hsic_2 - hsic_3
+	hsic_val = tf.maximum(0.0, hsic_val)
+	return hsic_val
 
 
 def auroc(labels, predictions):
@@ -83,8 +133,21 @@ def get_prediction_by_group(labels, predictions):
 
 	return mean_pred_dict
 
+def get_hsic_at_sigmas(sigma_list, labels, embedding, eager):
+	result_dict = {}
+	for sigma_val in sigma_list:
+		hsic_at_sigma = hsic(embedding, labels[:, 1:], sigma=sigma_val)
+		if eager:
+			result_dict[f'hsic{sigma_val}'] = hsic_at_sigma.numpy()
 
-def get_eval_metrics_dict(labels, predictions, params):
+		else:
+			result_dict[f'hsic{sigma_val}'] = tf.compat.v1.metrics.mean(
+				hsic_at_sigma)
+	return result_dict
+
+
+def get_eval_metrics_dict(labels, predictions, sample_weights, params, eager=False,
+	sigma_list=[0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]):
 	del params
 	y_main = tf.expand_dims(labels[:, 0], axis=-1)
 
@@ -94,5 +157,8 @@ def get_eval_metrics_dict(labels, predictions, params):
 
 	mean_pred_dict = get_prediction_by_group(labels, predictions["probabilities"])
 
-	return {**eval_metrics_dict, **mean_pred_dict}
+	hsic_val_dict = get_hsic_at_sigmas(sigma_list, labels,
+		predictions['embedding'], eager=eager)
+
+	return {**eval_metrics_dict, **mean_pred_dict, **hsic_val_dict}
 
