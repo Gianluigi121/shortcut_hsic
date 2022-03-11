282755755aa26ad542dfa7e519e9a6bcc8426f38
Jiayun Zheng <zhengji@armis2-login2.arc-ts.umich.edu>
2022-03-11 00:34:49 -0500

    Nearly done

--- age_model.py
+++ age_model.py
@@ -2,80 +2,85 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import Model
+from model import Resnet50, Densenet121
 import datetime
+import matplotlib.pyplot as plt
+import pickle
+from collections import OrderedDict
 
 from data_builder import create_save_chexpert_lists, load_ds_from_csv
-from evaluation import accuracy
+from evaluation import accuracy, accuracy_metric
 
 MAIN_DIR = "/nfs/turbo/coe-rbg/zhengji/age/"
-
-"""
-Model
-"""
-from tensorflow.keras.applications.resnet50 import ResNet50
-pre_trained_model = ResNet50(input_shape=(128, 128, 3), 
-                             include_top=False, 
-                             weights='imagenet')
-
-last_layer = pre_trained_model.get_layer('conv5_block3_3_conv')
-print('last layer output shape: ', last_layer.output_shape)
-last_output = last_layer.output 
-
-x = layers.Flatten()(last_output)
-x = layers.Dense(1024, activation='relu')(x)
-x = layers.Dropout(0.2)(x)                  
-x = layers.Dense(1, activation='linear')(x)  
-model = Model(pre_trained_model.input, x) 
-model.summary()
-
 params = {'batch_size': 16, 
-		  'epoch_num': 200,
+		  'epoch_num': 30,
 		  'pixel': 128}
+model = Resnet50(params)
+model.summary()
+acc_metric = accuracy_metric(name='ACC')
 
-# # STEP 1: Create the dataset csv
-# create_save_chexpert_lists(MAIN_DIR + 'data')
-# print("Finish creating the dataset csv")
-
-# # STEP 2: Create training, validation and testing dataset
+# Create training, validation and testing dataset
 data_dir = MAIN_DIR + 'data'
 train_ds, valid_ds, test_ds_dict = load_ds_from_csv(data_dir, False, params)
 print("Finish creating the training, validation, and testing dataset")
 
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-history = model.fit(train_ds, epochs=params['epoch_num'], validation_data=valid_ds)
+model.compile(optimizer='adam', 
+			  loss='mean_squared_error', 
+			  metrics=[acc_metric], 
+			  run_eagerly=True)
+history = model.fit(train_ds, 
+					epochs=params['epoch_num'], 
+					validation_data=valid_ds, 
+					verbose=1)
 
-test_ds = test_ds_dict[0.5]
-for x, y in test_ds.take(2):
-	pred = model(x)
-	print(f"prediction shape: {pred.shape}")	# should be (batch_size,)
-	error_rate = accuracy(pred, y, 2)
-	print(f"error rate: {error_rate}")
-# model.evaluate(test_ds_dict[0.5])
+"""Testing"""				
+test_loss_dict = OrderedDict()
+test_accuracy_dict = OrderedDict()
+for skew_prob in test_ds_dict:
+	test_ds = test_ds_dict[skew_prob]
+	res_list = model.evaluate(test_ds)
+	loss = res_list[0]
+	acc = res_list[1]
+	print(f"Result for Test datset with skew prob: {skew_prob}"
+		f'Test Loss: {loss}, '
+		f'Test Accuracy: {acc * 100}'
+		)
+	test_loss_dict[skew_prob] = loss
+	test_accuracy_dict[skew_prob] = acc
 
-import matplotlib.pyplot as plt
-# %matplotlib inline
-# acc = history.history['acc']
-# val_acc = history.history['val_acc']
+loss_file = open('loss.txt', 'wb')
+pickle.dump(test_loss_dict, loss_file)
+loss_file.close()
+
+accuracy_file = open("accuracy.txt", "wb")
+pickle.dump(test_accuracy_dict, accuracy_file)
+accuracy_file.close()
+
+"""Plot"""
+# Plot the training loss and validation loss
 loss = history.history['loss']
 val_loss = history.history['val_loss']
-
 epochs = range(len(loss))
-# fig1 = plt.gcf()
-# plt.plot(epochs, acc, 'r', label='Training accuracy')
-# plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
-# plt.title('Training and validation accuracy')
-# plt.legend(loc=0)
-# fig1.savefig(accuracy_plot)
-
-# plt.figure()
-# plt.show()
-
+plt.figure()
 fig1 = plt.gcf()
-plt.plot(epochs, loss, 'r', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.plot(epochs, loss, 'r', '--o', label='Training loss')
+plt.plot(epochs, val_loss, 'b', '--o', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend(loc=0)
 plt.savefig('age_plot.png')
 
-# plt.figure()
-# plt.show()
+# Test loss across different skewed datasets
+plt.figure()
+plt.plot(list(test_loss_dict.keys()), list(test_loss_dict.values()), "--o")
+plt.xlabel("skew probability")
+plt.ylabel("Loss")
+plt.title("Loss VS Skew Prob(Test dataset)")
+plt.savefig(MAIN_DIR + "test_loss.png")
+
+# Test accuracy across different skewed datasets
+plt.figure()
+plt.plot(list(test_accuracy_dict.keys()), list(test_accuracy_dict.values()), "--o")
+plt.xlabel("skew probability")
+plt.ylabel("Accuracy")
+plt.title("Accuracy VS Skew Prob(Test dataset)")
+plt.savefig(MAIN_DIR + "test_accuracy.png")
--- train.py
+++ train.py
@@ -2,10 +2,16 @@ import tensorflow as tf
 from model import Resnet50, Densenet121
 from data_builder import create_save_chexpert_lists, load_ds_from_csv
 from evaluation import accuracy, accuracy_metric
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from tqdm import tqdm
+
 
 params = {'batch_size': 32, 
-		  'epoch_num': 10,
-		  'pixel': 128}
+		  'epoch_num': 1,
+		  'pixel': 128
+         }
+
 MAIN_DIR = "/nfs/turbo/coe-rbg/zhengji/age/"
 data_dir = MAIN_DIR + 'data'
 train_ds, valid_ds, test_ds_dict = load_ds_from_csv(data_dir, False, params)
@@ -19,8 +25,8 @@ train_accuracy = accuracy_metric(name='train_accuracy')
 test_accuracy = accuracy_metric(name='test_accuracy')
 
 @tf.function
-def train_step(model, x, y, loss_object, train_loss, train_accuracy):
-    print("train_step")
+def train_step(model, x, y):
+    # print("train_step")
     with tf.GradientTape() as tape:
         pred = model(x)
         loss = loss_object(pred, y)
@@ -28,45 +34,80 @@ def train_step(model, x, y, loss_object, train_loss, train_accuracy):
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
     train_loss(pred, y)
-    train_accuracy(pred, predictions, threshold=2)
+    train_accuracy(pred, y)
 
 @tf.function
-def test_step(model, x, y, loss_object, test_loss, test_accuracy):
+def test_step(model, x, y):
     pred = model(x)
     test_loss = loss_object(pred, y)
+
     test_loss(pred, y)
-    test_accuracy(pred, predictions, threshold=2)
+    test_accuracy(pred, y)
     
 """Training and Validation"""
-EPOCHS = 1
-for epoch in range(EPOCHS):
-    # Reset the metrics at the start of the next epoch
-    train_loss.reset_states()
-    test_loss.reset_states()
-    train_accuracy.reset_states()
-    test_accuracy.reset_states()
+@tf.function
+def train_valid(params):
+    for epoch in range(params['epoch_num']):
+        # Reset the metrics at the start of the next epoch
+        train_loss.reset_states()
+        test_loss.reset_states()
+        train_accuracy.reset_states()
+        test_accuracy.reset_states()
 
-    for x, y in train_ds:
-        train_step(curr_model, x, y, loss_object, train_loss, train_accuracy)
+        # for x, y in train_ds:
+        #     correct_train_num = train_step(curr_model, x, y)
 
-    for x, y in valid_ds:
-        test_step(curr_model, x, y, loss_object, test_loss, test_accuracy)
+        for x, y in valid_ds:
+            correct_valid_num = test_step(curr_model, x, y)
 
-    print(
-        f'Epoch {epoch + 1}, '
-        f'Loss: {train_loss.result()}, '
-        f'Train Accuracy: {train_accuracy.result() * 100}, '
-        f'Valid Loss: {test_loss.result()}, '
-        f'Valid Accuracy: {test_accuracy.result() * 100}'
-    )
+        print(
+            f'Epoch {epoch + 1}, '
+            f'Loss: {train_loss.result()}, '
+            f'Train Accuracy: {train_accuracy.result() * 100}, '
+            f'Valid Loss: {test_loss.result()}, '
+            f'Valid Accuracy: {test_accuracy.result() * 100}'
+        )
+
+tf.config.run_functions_eagerly(True)
+train_valid(params)
+print(tf.__version__)
 
 """Testing"""
-for skew_prob in test_ds_dict:
-    test_ds = test_ds_dict[skew_prob]
-    for x, y in test_ds:
-        test_step(model, x, y, loss, test_loss)
-    print(f"Result for Test datset with skew prob: {skew_prob}"
-          f'Test Loss: {test_loss.result()}, '
-          f'Test Accuracy: {test_accuracy.result() * 100}')
+@tf.function
+def test():
+    test_loss_dict = {}
+    test_accuracy_dict = {}
+    for skew_prob in test_ds_dict:
+        test_loss.reset_states()
+        test_accuracy.reset_states()
 
+        test_ds = test_ds_dict[skew_prob]
+        for x, y in test_ds:
+            test_step(curr_model, x, y)
+        print(f"Result for Test datset with skew prob: {skew_prob}"
+            f'Test Loss: {test_loss.result()}, '
+            f'Test Accuracy: {test_accuracy.result() * 100}'
+            )
+        test_loss_dict[skew_prob] = test_loss.result()
+        test_accuracy_dict[skew_prob] = test_accuracy.result()
+    test_loss_dict = sorted(test_loss_dict.items())
+    test_accuracy_dict = sorted(test_accuracy_dict.items())
+    return test_loss_dict, test_accuracy_dict
 
+test_loss_dict, test_accuracy_dict = test()
+"""Plot"""
+# Test loss across different skewed datasets
+plt.figure()
+plt.plot(test_loss_dict.keys, test_loss_dict.values, "--o")
+plt.xlabel("skew probability")
+plt.ylabel("Loss")
+plt.title("Loss VS Skew Prob(Test dataset)")
+plt.savefig(MAIN_DIR + "test_loss.png")
+
+# Test accuracy across different skewed datasets
+plt.figure()
+plt.plot(test_accuracy_dict.keys, test_accuracy_dict.values, "--o")
+plt.xlabel("skew probability")
+plt.ylabel("Accuracy")
+plt.title("Accuracy VS Skew Prob(Test dataset)")
+plt.savefig(MAIN_DIR + "test_accuracy.png")
