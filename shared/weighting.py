d3e215d23d3f477198eb9c0aec2962095378c1f9
mymakar <maggieymakar@gmail.com>
2022-04-28 13:01:16 -0400

    added permutation weighting

--- shared/weighting.py
+++ shared/weighting.py
@@ -1,17 +1,19 @@
 """weighting methods."""
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 pd.options.mode.chained_assignment = None
 
 def get_binary_weights(data, data_type, weighting_type):
 	# --- load data
 	data = data['0'].str.split(",", expand=True)
-	
+
 	if data_type == 'chexpert':
 		D = data.shape[1] -1
-		data.columns = ['file_name'] +  [f'y{i}' for i in range(D)]
+		data.columns = ['file_name'] + [f'y{i}' for i in range(D)]
 	elif data_type == 'waterbirds':
-		D = data.shape[1]-4
+		D = data.shape[1] - 4
 		data.columns = ['bird_img', 'bird_seg', 'back_img', 'noise_img'] + \
 			[f'y{i}' for i in range(D)]
 
@@ -39,8 +41,8 @@ def get_binary_weights(data, data_type, weighting_type):
 			pv = np.mean(pv)
 			if weighting_type == "tr_consistent":
 				num =  py * pv
-			else: 
-				num = pv 
+			else:
+				num = pv
 			data['weights'] = mask * (num/denom) + (1 - mask) * data['weights']
 
 	# print(data.weights.isnull().sum())
@@ -48,12 +50,65 @@ def get_binary_weights(data, data_type, weighting_type):
 	if data_type == 'chexpert':
 		txt_data = data.file_name
 
-	elif data_type == 'waterbirds': 
+	elif data_type == 'waterbirds':
 		txt_data = data.bird_img + \
 			',' + data.bird_seg + \
 			',' + data.back_img + \
 			',' + data.noise_img
-	
+
+	for i in range(D):
+		txt_data = txt_data + ',' + data[f'y{i}'].astype(str)
+
+	txt_data = txt_data + ',' + data.weights.astype(str)
+
+	txt_data = txt_data.apply(lambda x: [x])
+	return txt_data
+
+
+def get_permutation_weights(data, data_type, weighting_type):
+	rng = np.random.RandomState(0)
+	# --- load data
+	data = data['0'].str.split(",", expand=True)
+	if data_type == 'dr':
+		D = data.shape[1] - 2
+		data.columns = ['img_name', 'noise_file'] + [f'y{i}' for i in range(D)]
+
+	elif data_type == 'waterbirds':
+		D = data.shape[1] - 4
+		data.columns = ['bird_img', 'bird_seg', 'back_img', 'noise_img'] + \
+			[f'y{i}' for i in range(D)]
+
+	for i in range(D):
+		data[f'y{i}'] = data[f'y{i}'].astype(np.float32)
+
+	# --- create permuted dataset
+	obs_data = data[[f'y{i}' for i in range(D)]].copy()
+	obs_data['label'] = 0
+
+	perm_data = data[[f'y{i}' for i in range(D)]].copy()
+	perm_y0 = perm_data['y0'].values
+	perm_y0 = rng.choice(perm_y0, size=len(perm_y0), replace=False)
+	perm_data['y0'] = perm_y0
+	perm_data['label'] = 1
+
+	weighting_data = pd.concat([obs_data, perm_data], axis=0)
+
+	# clf = LogisticRegression(penalty='none')
+	clf = RandomForestClassifier(random_state=0)
+	clf.fit(weighting_data[[f'y{i}' for i in range(D)]],
+		weighting_data.label)
+	data['weights'] = clf.predict_proba(data[[f'y{i}' for i in range(D)]])[:, 1]
+	data['weights'] = data['weights'] / (1.0 - data['weights'])
+
+	if data_type == 'dr':
+		txt_data = data.img_name + \
+			',' + data.noise_file
+	elif data_type == 'waterbirds':
+		txt_data = data.bird_img + \
+			',' + data.bird_seg + \
+			',' + data.back_img + \
+			',' + data.noise_img
+
 	for i in range(D):
 		txt_data = txt_data + ',' + data[f'y{i}'].astype(str)
 
